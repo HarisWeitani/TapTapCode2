df474c6e18390a2c2b854e1c261e97054824407d
Albert Miliano <nexstepx@gmail.com>
2017-03-24 14:15:16 +0700

    Hardware

---
+++ Assets/_Script/CameraController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+
+    public bool CameraHardware;
+	
+	void Start () {
+        CameraHardware = false;
+	}
+	
+
+	void Update () {
+
+        if (CameraHardware == true)
+        {
+            animation.
+        }
+
+	}
+}
