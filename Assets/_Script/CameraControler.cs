7d60e799d6d47030a344ebd89a4d300d24494c95
Albert Miliano <nexstepx@gmail.com>
2017-04-05 21:00:07 +0700

    SFX, Back Button
    done

---
+++ Assets/_Script/CameraControler.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraControler : MonoBehaviour
+{
+
+
+    public void moveCameraToHardware()
+    {
+        this.transform.position = new Vector3((float)-6.25, 0, -10);
+    }
+
+    public void moveCameraToSoftware()
+    {
+        this.transform.position = new Vector3((float)6.25, 0, -10);
+    }
+
+    public void moveCameraToMain()
+    {
+        this.transform.position = new Vector3(0, 0, -10);
+    }
+
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+
+
+    }
+}
--- Assets/_Script/Sound.cs
+++ Assets/_Script/Sound.cs
@@ -4,13 +4,52 @@ using UnityEngine;
 
 public class Sound : MonoBehaviour {
 
+    public AudioClip keyboard1, keyboard2, keyboard3;
+    private float timeKeySounds;
+    private AudioSource keyIdle;
+    private int count;
+
 	// Use this for initialization
 	void Start () {
-		
+        timeKeySounds = 1;
+        keyIdle = GetComponent<AudioSource>();
+        count = 0;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+
+        timeKeySounds -= Time.deltaTime;
+
+        if (Input.GetMouseButtonDown(0))
+        {
+
+            keyIdle.Pause();
+
+            if (count == 0)
+            {
+                keyIdle.PlayOneShot(keyboard1);
+                count = 1;
+            }
+
+            else if (count == 1)
+            {
+                keyIdle.PlayOneShot(keyboard2);
+                count = 2;
+            }
+
+            else if (count == 2)
+            {
+                keyIdle.PlayOneShot(keyboard3);
+                count = 0;
+            }
+
+            if (timeKeySounds < 0)
+            {
+                keyIdle.Play();
+            }
+
+
+        }
 	}
 }
