5d408e3cadb8c2a067ff5e520b0ee09e7a221cd3
Haris Weitani <hweitani@gmail.com>
2017-05-08 13:42:24 +0700

    Cancer PopUp text ADDED

---
+++ Assets/_Script/popUpText.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class popUpText : MonoBehaviour
+{
+    public GameObject popUp;
+    public Canvas canvas;
+    private Vector2 position = new Vector2(0, 0);
+
+    void Start()
+    {
+        GameObject inst = (GameObject)Instantiate(popUp, position, Quaternion.identity);
+        inst.transform.SetParent(canvas.transform);
+        //Destroy(inst, 15.0f);
+    }
+    public void PopUp()
+    {
+
+    }
+
+}
